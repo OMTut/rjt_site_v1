4e3ee1185896dbee93dd7edab0a5a34862113372
OMTut <pzx4@txstate.edu>
2024-01-03 12:53:46 -0600

    added temporary login states

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -27,11 +27,28 @@ const Links = () => {
         },
     ];
 
+    //dummy session/auth data
+    const session = true;
+    const isAdmin = true;
+
     return (
         <div className={styles.links}>
             {links.map((link=>(
                 <NavLink item ={link} key={link.title}/>
-            )))}
+            )))} {
+                session ? (
+                    <>
+                    {
+                        isAdmin && (
+                            <NavLink item={{title: 'Admin', path: '/admin'}}/>
+                        )
+                    }
+                    <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{ title: 'Login', path: '/login'}}/>
+                )
+            }
         </div>
 
     )
