e6acfc5cdc6dffee0c59892345d7cf7316ff4048
OMTut <pzx4@txstate.edu>
2024-01-08 10:13:37 -0600

    started form design

--- src/app/inquire/page.jsx
+++ src/app/inquire/page.jsx
@@ -1,7 +1,19 @@
+import styles from "./inquire.module.css"
 const InquirePage = () => {
     return (
-      <div>
-        <h1>Inquirepage</h1>
+      <div className={styles.inquireContainer}>
+        <div className={styles.leftContainer}>
+          <p>Some text or something will go here</p>
+        </div>
+        <div className={styles.formContainer}>
+          <form action="" className={styles.form}>
+            <input type='text' placeholder='Name'/>
+            <input type='text' placeholder='Email'/>
+            <input type='text' placeholder='Phone (Optional)'/>
+            <textarea name='' id='' cols='30' rows='10' placeholder='Message'></textarea>
+            <button>Send</button>
+          </form>
+        </div>
       </div>
     );
   }
