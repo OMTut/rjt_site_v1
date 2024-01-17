cf3f2a7c83172b339ca53a539fb0b31faa006102
OMTut <pzx4@txstate.edu>
2024-01-17 12:28:02 -0600

    added error handling to getPost/s

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,12 +1,22 @@
 const pool = require('./utils')
 
 export const getPosts = async () => {
-    const res = await pool.query('SELECT * FROM thoughts');
-    return res.rows;
+    try {
+        const res = await pool.query('SELECT * FROM thoughts');
+        return res.rows;
+    } catch (err) {
+        console.log(err);
+        throw new Error('Failed to fetch Posts');
+    }
 }
 
 //returns a single post by id
 export const getPost = async (id) => {
-    const res = await pool.query('SELECT * FROM thoughts WHERE id = $1', [id]);
-    return res.rows[0];
+    try {
+        const res = await pool.query('SELECT * FROM thoughts WHERE id = $1', [id]);
+        return res.rows[0];
+    } catch {
+        console.log(err);
+        throw new Error('Failed to fetch Post');
+    }
 }
